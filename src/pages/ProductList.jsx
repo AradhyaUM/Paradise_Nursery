846c0e98c1b69739d37ed2f9cc06e2050c393368
QuantaG <quantaramg@gmail.com>
2026-04-20 23:16:27 +0530

    Final project update with images and UI

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,7 +5,7 @@ export default function ProductList() {
   const products = [
     { id: 1, name: "Lavender", price: 15, image: "/Paradise_Nursery/images/lavender.png" },
     { id: 2, name: "Mint", price: 10, image: "/Paradise_Nursery/images/mint.png" },
-    { id: 3, name: "Aloe Vera", price: 12, image: "/Paradise_Nursery/images/aloe.png" },
+    { id: 3, name: "Aloe Vera", price: 12, image: "/Paradise_Nursery/images/alovera.png" },
     { id: 4, name: "Tulsi", price: 14, image: "/Paradise_Nursery/images/tulsi.png" },
     { id: 5, name: "Cactus", price: 8, image: "/Paradise_Nursery/images/cactus.png" },
     { id: 6, name: "Succulent", price: 18, image: "/Paradise_Nursery/images/succulent.png" }
